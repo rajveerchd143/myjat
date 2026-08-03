f1c8a8a7e41342ce96add6c34f4534c16fc16649
Rajveer Singh <rajveerchd143@gmail.com>
2026-08-04 00:42:24 +0530

    Auto Update 2026-08-04 00:42:24

--- includes/membership-google-login.php
+++ includes/membership-google-login.php
@@ -14,7 +14,7 @@ if (!defined('ABSPATH')) {
 */
 
 
-
+require_once __DIR__ . '/membership-config.php';
 /*
 |--------------------------------------------------------------------------
 | Configuration
